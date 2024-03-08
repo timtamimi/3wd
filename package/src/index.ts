ad666ac698e7e63b19cd6756e5fa09073a3c0df8
Tim <tim@tamimi.dev>
2024-03-08 21:42:56 +0000

    lint + test

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,17 +1,19 @@
-import dataset from './master.json'
+import dataset from "./master.json";
 
-const ERROR_STRING = 'error.error.error'
+const ERROR_STRING = "error.error.error";
 
 export function parseDate(date: Date): string {
-  const dateAsFormattedString = date.toISOString().split('T')[0];
-  const returnableValueFromDataset = dataset[dateAsFormattedString as keyof typeof dataset];
+  const dateAsFormattedString = date.toISOString().split("T")[0];
+  const returnableValueFromDataset =
+    dataset[dateAsFormattedString as keyof typeof dataset];
   return returnableValueFromDataset || ERROR_STRING;
 }
 
-const getKeyByValue = (obj: Object, value: any): string => 
-        Object.keys(obj).find(key => obj[key as keyof typeof obj] === value) || "error";
+const getKeyByValue = (obj: Object, value: any): string =>
+  Object.keys(obj).find((key) => obj[key as keyof typeof obj] === value) ||
+  "error";
 
 export function parseString(inputString: string): Date {
-  const value = getKeyByValue(dataset, inputString)
+  const value = getKeyByValue(dataset, inputString);
   return new Date(value);
 }
--- package/tests/add.test.ts
+++ package/tests/add.test.ts
@@ -1,6 +1,20 @@
-import { add } from '../src';
+import { parseDate, parseString } from "../src";
 
-test('adds two numbers correctly', () => {
-  const result = add(2, 3);
-  expect(result).toBe(5);
+describe("parseDate", () => {
+  test("parses a known date", () => {
+    const result = parseDate(new Date("2000-01-01"));
+    expect(result).toBe("toe.engine.bells");
+  });
+
+  test("returns an error for an unknown date", () => {
+    const result = parseDate(new Date("1010-01-01"));
+    expect(result).toBe("error.error.error");
+  });
+});
+
+describe("parseString", () => {
+  test("parses a known string", () => {
+    const result = parseString("toe.engine.bells");
+    expect(result).toStrictEqual(new Date("2000-01-01"));
+  });
 });
